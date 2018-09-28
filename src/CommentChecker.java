e14e74be96704376b3c23612440943d725442e11
Hugo Yu <guo.yu@uwaterloo.ca>
2018-09-28 14:01:25 -0400

    read in file fix

--- src/CommentChecker.java
+++ src/CommentChecker.java
@@ -9,29 +9,31 @@ public class CommentChecker {
 
         // check input arguments
         if (args.length != 1) {
-            System.err.println("Usage: java CommentChecker [ filename ]");
+            System.err.println("Usage: java CommentChecker [ filepath ]");
             return;
         }
 
         // open file
-        String filename = args[0];
+        String filePath = args[0];
         BufferedReader br;
         try {
             br = new BufferedReader(new FileReader(args[0]));
         } catch (FileNotFoundException e) {
-            System.err.println("File not found for file name: " + filename);
+            System.err.println("File not found: " + filePath);
             return;
         }
 
+        String[] fileDirs = filePath.split("/");
+        String fileName = fileDirs[fileDirs.length - 1];
         // ignore file names start with . and file names without extension
-        if (filename.startsWith(".") || !filename.contains(".")) {
-            System.out.println("Ignored file: " + filename);
+        if (fileName.startsWith(".") || !filePath.contains(".")) {
+            System.out.println("Ignored file: " + filePath);
             return;
         }
 
         // check extension and construct corresponding lexer object
         Lexer lexer = null;
-        String extension = filename.substring(filename.lastIndexOf('.') + 1).toLowerCase();
+        String extension = fileName.substring(fileName.lastIndexOf('.') + 1).toLowerCase();
         switch (extension) {
             case "c":
             case "cc":
@@ -62,10 +64,10 @@ public class CommentChecker {
                 System.out.println("Total # of TODO’s: " + res.numTodo);
             } catch (IOException e) {
                 e.printStackTrace();
-                System.err.println("Error reading file: " + filename);
+                System.err.println("Error reading file: " + filePath);
             }
         } else {
-            System.out.println("File not supported: " + filename);
+            System.out.println("File not supported: " + filePath);
             return;
         }
 
